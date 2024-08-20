9f9d5502a8a1d4c049898884992ab48cf8a61ab6
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-20 13:43:59 +0700

    init

--- src/main/java/com/banyuijio/auth/repository/UserInternalRepository.java
+++ src/main/java/com/banyuijio/auth/repository/UserInternalRepository.java
@@ -3,10 +3,12 @@ package com.banyuijio.auth.repository;
 import com.banyuijio.auth.entity.UserInternal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserInternalRepository extends JpaRepository<UserInternal, UUID> {
 
     UserInternal findByUserId(UUID userId);
+    Optional<UserInternal> findByUsername(String userName);
 
 }
--- src/main/java/com/banyuijio/auth/service/auth/AuthServiceImpl.java
+++ src/main/java/com/banyuijio/auth/service/auth/AuthServiceImpl.java
@@ -2,14 +2,26 @@ package com.banyuijio.auth.service.auth;
 
 import com.banyuijio.auth.dto.user.UserInternalLoginInput;
 import com.banyuijio.auth.dto.user.UserInternalLoginOutput;
+import com.banyuijio.auth.entity.UserInternal;
+import com.banyuijio.auth.enums.HttpStatusCode;
+import com.banyuijio.auth.exception.HttpStatusException;
+import com.banyuijio.auth.repository.UserInternalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+    private final UserInternalRepository userInternalRepository;
     @Override
     public UserInternalLoginOutput loginInternal(UserInternalLoginInput input) {
-        return null;
+        UserInternalLoginOutput output = new UserInternalLoginOutput();
+        UserInternal user = userInternalRepository.findByUsername(input.getUsername())
+                .orElseThrow(()->new HttpStatusException(HttpStatusCode.UNAUTHORIZED));
+        if (user.getPassword().equalsIgnoreCase(input.getPassword())){
+            output.setUserId(user.getUserId());
+            output.setLoginId(user.getUsername());
+        }
+        return output;
     }
 }
